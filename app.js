a69edf1a9244394156b99822724df146eae279d3
MessaoudBahaa <fm_berbache@esi.dz>
2019-08-28 16:49:26 +0100

    adding body-parser lib + some comments

--- app.js
+++ app.js
@@ -1,34 +1,31 @@
-
 var express = require("express");
 var mysql = require("mysql");
 var app = express();
-app.use(express.static('public'));
+app.use(express.static("public"));
+var bodyparser = require("body-parser");
+app.use(bodyparser.json());
 
-
+//database connection
 var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database:'pharma_db'
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "pharma_db"
 });
 connection.connect();
 
-app.get('/getusers',function(req,res){  
-    var query = "select * from users ";
-    connection.query(query,function(error,results){
+// Server creation
+var server = app.listen(3000, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+});
+console.log("Node server running on port 3000");
+
+// Rest service getting all the users
+app.get("/getusers", function(req, res) {
+  var query = "select * from users ";
+  connection.query(query, function(error, results) {
     if (error) throw error;
     res.send(JSON.stringify(results));
-
-})
+  });
 });
-
-
-var server = app.listen(3000,function(){
-    var host = server.address().address
-    var port = server.address().port
-});
-console.log('Node server running on port 3000');
-
-
-
-
